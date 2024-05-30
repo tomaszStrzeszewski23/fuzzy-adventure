25074d5efcfbed2641cf2cb9723ed5931543eed5
Tomasz Strzeszewski <tomasz.strzeszewski@toptal.com>
2024-05-30 20:32:01 +0200

    change curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ then
     DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
     DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
 
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
+    URL="https://api.github.com/repos/tomaszStrzeszewski23/gh_actions_1/releases"
 
     if [[ "${LOCAL_TEST}" == *"true"* ]];
     then
