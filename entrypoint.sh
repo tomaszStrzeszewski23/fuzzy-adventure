193b02a29ecc448779400e5f435ea30eac7cb3c6
Tomasz Strzeszewski <tomasz.strzeszewski@toptal.com>
2024-05-30 20:26:17 +0200

    change curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ then
     then
         echo "## [TESTING] Keyword was found but no release was created."
     else
-        echo $DATA | http POST $URL | jq .
+        echo $DATA | curl -H "Authorization: ${GITHUB_TOKEN}" $URL | jq .
     fi
 # otherwise
 else
